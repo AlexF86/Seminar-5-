f7ca757e690319817389ed975de55d2a2b66e47c
Alexandr <don.alexande1986@yandex.ru>
2022-08-26 15:44:10 +0300

    Задача 32

---
+++ Tasks/Task 32/Program.cs
@@ -0,0 +1,25 @@
+﻿int n = 4;
+int[] CreateArray(int n)
+{
+    int[] array = new int[] { -4, -8, 8, 2};
+    return array;
+}
+int[] GetOppositArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = array[i] * (-1);
+    }
+    return array;
+}
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        System.Console.Write($"{arr[i]} ");
+    }
+    System.Console.WriteLine();
+}
+int[] array = CreateArray(n);
+PrintArray(array);
+PrintArray(GetOppositArray(array));
