62613d49c42a05316828649a2e36d690b7c78efe
Alexandr <don.alexande1986@yandex.ru>
2022-08-26 16:59:33 +0300

    Задача 35

--- Tasks/Task 35/Program.cs
+++ Tasks/Task 35/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] array = new int[123];
+int count=0;
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 200);
+    Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+for (int j = 0; j < array.Length; j++)
+{
+    if(array[j] > 0 & array[j] < 200) count = count + 1;
+}
+Console.WriteLine();
+Console.Write("Количество чисел на отрезке [10,99] - ");
+Console.Write(count);
