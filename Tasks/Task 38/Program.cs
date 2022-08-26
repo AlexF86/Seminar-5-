fd532f8969a8ae9f3a6b12435add17e2bed39137
Alexandr <don.alexande1986@yandex.ru>
2022-08-26 19:41:16 +0300

    задача 38

--- Tasks/Task 38/Program.cs
+++ Tasks/Task 38/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] array = {3, 7, 22, 2, 78};
+int max = array[0];
+int min = array[0];
+
+for (int j = 0; j < array.Length; j++)
+{
+    if (array[j] > max) max = array[j];
+    if (array[j] < min) min = array[j];
+}
+Console.WriteLine();
+Console.Write("Разница между максимальным и минимальным элементом: ");
+Console.Write(max - min);
